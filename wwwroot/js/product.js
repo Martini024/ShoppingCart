bfb6217eced92384272848e2b9711ea7e9b99ee9
martini <zhao435021640@gmail.com>
2020-04-11 22:53:04 +0800

    add fontawesome icon

--- wwwroot/js/product.js
+++ wwwroot/js/product.js
@@ -1,15 +1,15 @@
 $('.addToCart').click(function (e) {
-	// get current clicked product
 	var productId = $(this).attr('value');
 	var price = $(this).text().substring(1, $(this).text().indexOf(' '));
+	e.preventDefault();
 	$.ajax({
 		type: 'POST',
 		contentType: 'application/json',
 		url: '/Cart/AddToCart?' + 'ProductId=' + productId + '&Price=' + price,
 		success: function (res) {
-			console.log(res);
-
-			$('#totalQty').html(res);
+			$('#totalQty')
+				.empty()
+				.append('<i class="fas fa-cart-plus"></i>', ' ' + res);
 		},
 	});
 });
