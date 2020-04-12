f121b6f5a6b14e5b21365e4199a75c3137b833df
martini <zhao435021640@gmail.com>
2020-04-13 00:41:20 +0800

    add a confirm window to give user option of merging cart

--- wwwroot/js/auth.js
+++ wwwroot/js/auth.js
@@ -2,19 +2,25 @@ $('#signIn').on('submit', function (e) {
 	e.preventDefault();
 	var userName = $('input[name=userName').val();
 	var password = md5($('input[name=password').val());
+	var merge = true;
 	if (localStorage.getItem('cart') !== null) {
 		var cart = JSON.parse(localStorage.getItem('cart'));
 		var cartDetails = cart.cartDetails;
+		if (cartDetails.length > 0) {
+			merge = confirm('Do you want to merge cart into your account?');
+		}
 		cartDetails = JSON.stringify(cartDetails);
-		console.log(cartDetails);
 	}
+
+	var postBody = '';
+	if (merge) postBody = cartDetails;
 
 	$.ajax({
 		type: 'POST',
 		dataType: 'text',
 		contentType: 'application/json; charset=utf-8',
 		url: '/Auth/Login?' + 'userName=' + userName + '&password=' + password,
-		data: cartDetails,
+		data: postBody,
 		success: function (res) {
 			localStorage.removeItem('cart');
 			window.location.href = res;
